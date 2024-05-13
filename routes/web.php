d1d89a2577ff505e47051a8f02328896c7631b92
Elsha Adrianti <elshaadrianti@gmail.com>
2024-05-12 21:37:59 -1100

    create views for dashboard, devices, rules, users, and edit the routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+Route::get('/devices', function () {
+    return view('devices');
+});
+Route::get('/rules', function () {
+    return view('rules');
+});
+Route::get('/users', function () {
+    return view('users');
 });
